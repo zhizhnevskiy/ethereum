f227ddc2203517ee36f831ab6d194e1a8de911ca
zhizhnevskiy <zhizhnevskiydev@gmail.com>
2021-09-17 17:38:05 +0300

    update project

---
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// const SearchBar = () => {
+//     return <input />;
+// };
+
+class SearchBar extends Component {
+    render() {
+        return <input />;
+    }
+}
+
+export default SearchBar;
--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyA8ZBKA9vkzI1CJc9SIT1shD8iaGXxMpGs';
+
 // Create a react component.
 // This component should produce some HTML
 const App = () => {
-    return <div>Hi Yuriy!!!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take this component`s generated HTML
 // and put it on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App/>, document.querySelector('.container'));

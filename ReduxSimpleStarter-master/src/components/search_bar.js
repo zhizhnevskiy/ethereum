a6a456d696a00cc0ea98eae8ac3d344bf9ad3d21
Yuriy Zhizhnevskiy <yury.zhyzhneuski@innowise-group.com>
2021-09-18 20:53:34 +0300

    update project

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 // const SearchBar = () => {
 //     return <input />;
 // };
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''};
+    }
+
     render() {
-        return <input />;
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({term: event.target.value})}
+                />
+                Value of the input: {this.state.term}
+            </div>
+        );
     }
 }
 
--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
+//import from node_modules
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
+//import components
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
+//api key from you tube
 const API_KEY = 'AIzaSyA8ZBKA9vkzI1CJc9SIT1shD8iaGXxMpGs';
 
 // Create a react component.
 // This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+           this.setState({ videos: videos });
+        });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 // Take this component`s generated HTML

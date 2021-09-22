bd3e3403715537f3e992747a2e6921aa0aefd63c
zhizhnevskiy <zhizhnevskiydev@gmail.com>
2021-09-22 12:19:55 +0300

    update project

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,46 @@
+import web3 from './web3';
+
+const adress = '0x3bbc6A38364462d82B89e1489F70859af0958C43';
+const abi = [{
+    "constant": true,
+    "inputs": [],
+    "name": "manager",
+    "outputs": [{"name": "", "type": "address"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [{"name": "", "type": "address[]"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{"name": "", "type": "uint256"}],
+    "name": "players",
+    "outputs": [{"name": "", "type": "address"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {"inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor"}];
+
+export default new web3.eth.Contract(abi, adress);
--- lottery/deploy.js
+++ lottery/deploy.js
@@ -1,24 +1,24 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const {interface, bytecode} = require("./compile");
 
 const provider = new HDWalletProvider(
-  "grape large balcony cage spray grab coyote road viable end angle fetch",
-  // remember to change this to your own phrase!
-  "https://rinkeby.infura.io/v3/5966f6cabc04491098efb6d410b2eda0"
-  // remember to change this to your own endpoint!
+    "grape large balcony cage spray grab coyote road viable end angle fetch",
+    "https://rinkeby.infura.io/v3/5966f6cabc04491098efb6d410b2eda0"
 );
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
+    const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from account", accounts[0]);
+    console.log("Attempting to deploy from account", accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
-    .send({ gas: "1000000", gasPrice: '50000000000', from: accounts[0] });
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode})
+        .send({gas: "1000000", gasPrice: '50000000000', from: accounts[0]});
 
-  console.log("Contract deployed to", result.options.address);
+    console.log(interface);
+    console.log("Contract deployed to", result.options.address);
+
 };
 deploy();

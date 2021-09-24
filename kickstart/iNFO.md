419688c49c75f72ab2f64a1136e9ef4b663318fd
zhizhnevskiy <zhizhnevskiydev@gmail.com>
2021-09-24 15:10:19 +0300

    update project

--- kickstart/iNFO.md
+++ kickstart/iNFO.md
@@ -6,9 +6,11 @@ npm install --save ganache-cli
 npm install --save mocha
 npm install --save solc
 npm install --save fs-extra
+npm install --save truffle-hdwallet-provider@0.0.3
 # special for this project
 npm install --save web3@1.3.5
 npm install --save solc@0.4.17
+npm install --save truffle-hdwallet-provider@0.0.3
 # start test in kickstart
 npm run test
 ```
--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -33,16 +33,20 @@ beforeEach(async () => {
 });
 
 describe('Campaigns', () => {
+
     it('deploys a factory', () => {
         assert.ok(factory.options.address);
     });
+
     it('deploys a campaign', () => {
         assert.ok(campaign.options.address);
     });
+
     it('marks caller as the campaign manager', async () => {
         const manager = await campaign.methods.manager().call();
         assert.strictEqual(accounts[0], manager);
     });
+
     it('allows people to contribute money and marks them as approvers', async () => {
         await campaign.methods.contribute().send({
             value: '200',
@@ -51,6 +55,7 @@ describe('Campaigns', () => {
         const isContributor = await campaign.methods.approvers(accounts[1]).call();
         assert(isContributor);
     });
+
     it('requeres a minimum contribution', async () => {
         try {
             await campaign.methods.contribute().send({
@@ -61,6 +66,7 @@ describe('Campaigns', () => {
         } catch (err) {
         }
     });
+
     it('allows a manager to make a payment request', async () => {
         await campaign.methods
             .createRequest('Buy something', '100', accounts[1])
@@ -70,5 +76,33 @@ describe('Campaigns', () => {
             });
         const request = await campaign.methods.requests(0).call();
         assert.strictEqual('Buy something', request.description);
+    });
+
+    it('processes requests', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei('10', 'ether')
+        });
+        await campaign.methods
+            .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+            .send({
+                from: accounts[0],
+                gas: '1000000'
+            });
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        let balance = await web3.eth.getBalance(accounts[1]);
+        balance = web3.utils.fromWei(balance, 'ether');
+        balance = parseFloat(balance);
+        console.log(balance);
+        assert(balance > 104);
     });
 });

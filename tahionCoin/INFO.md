3f1c9ceb2132a619ef893926d6f5f0f591e0d7a4
zhizhnevskiy <zhizhnevskiydev@gmail.com>
2021-10-04 13:03:16 +0300

    update project

--- tahionCoin/INFO.md
+++ tahionCoin/INFO.md
@@ -4,22 +4,24 @@
 npm init
 npm install
 npm install --save ganache-cli mocha solc fs-extra
-# special for this project
-npm install --save web3@1.3.5 solc@0.4.17 truffle-hdwallet-provider@0.0.3
-# start test (kickstart folder)
-npm run test
-# deploy contract in Rinkeby (ethereum folder)
-node deploy.js
-
 # Add react with next
 npm install --save next react react-dom
+# Add next-routes
+npm install --save next-routes
 # Add css library Semantic
 npm install --save semantic-ui-react semantic-ui-css
+# special for this project
+npm install --save web3@1.3.5 solc@0.4.17 truffle-hdwallet-provider@0.0.3
+npm install next-routes --legacy-peer-deps
 # Changes in package.json  
   "scripts": {
     "test": "mocha",
     "dev": "next dev"
   },
+# start test (kickstart folder)
+npm run test
+# deploy contract in Rinkeby (ethereum folder)
+node deploy.js  
 # Run project with NextJS (tahionCoin folder)
 npm run dev
 ```
--- tahionCoin/pages/campaigns/new.js
+++ tahionCoin/pages/campaigns/new.js
@@ -1,23 +1,33 @@
 import React, {Component} from "react";
-import {Form, Button, Input} from "semantic-ui-react";
+import {Form, Button, Input, Message} from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import factory from "../../ethereum/factory";
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: '',
+        loading: false
     }
 
     onSubmit = async (event) => {
         event.preventDefault();
 
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods
-            .createCampaign(this.state.minimumContribution)
-            .send({
-                from: accounts[0]
-            });
+        this.setState({loading: true, errorMessage:''});
+
+        try{
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                });
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
+
+        this.setState({loading: false});
     }
 
     render() {
@@ -25,7 +35,7 @@ class CampaignNew extends Component {
             <Layout>
                 <h3>Create a Campaign</h3>
 
-                <Form onSubmit={this.onSubmit}>
+                <Form onSubmit={this.onSubmit} error={ !!this.state.errorMessage }>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input
@@ -36,8 +46,13 @@ class CampaignNew extends Component {
                                 this.setState({minimumContribution: event.target.value})}
                         />
                     </Form.Field>
-
+                    <Message
+                        error
+                        header='Error!'
+                        content={ this.state.errorMessage}
+                    />
                     <Button
+                        loading={this.state.loading}
                         primary
                         type='submit'
                     >
